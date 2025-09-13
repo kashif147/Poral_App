73f3f528abfc2067c9e8e5d0e082e3088e1630d4
FazalAzim <fazalazim238@gmail.com>
2025-09-13 18:59:24 +0500

    feature/Configuration of policy evaluation api

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import LandingPage from './src/modules/landing/LandingPage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { setBearerToken } from './src/helpers/auth.helper';
 import { signInWithAzureB2C } from './src/helpers/appAuth.helper';
+import { createPolicyEvaluationRequest } from './src/api/policy.Evaluation.api';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
@@ -19,6 +20,20 @@ function App() {
     const checkAuth = async () => {
       try {
         const token = await AsyncStorage.getItem('token');
+        if (token) {
+          createPolicyEvaluationRequest({
+            token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjg4OGI1ZGM2MGM3OThiMDk3ZTg2YzkxIiwidXNlckVtYWlsIjoiZmF6YWxhemltMjM4QGdtYWlsLmNvbSIsInVzZXJGdWxsTmFtZSI6IkZhemFsIEF6aW0iLCJ1c2VyTWljcm9zb2Z0SWQiOiJhOWY3YzkzMS03MTA0LTQyMGItYTdiNS00MjAzMjc4YjliNGMiLCJ1c2VyTWVtYmVyTnVtYmVyIjpudWxsLCJ1c2VyTW9iaWxlUGhvbmUiOm51bGwsInVzZXJQb2xpY3kiOiJCMkNfMV9wcm9qZWN0c2hlbGwiLCJ1c2VySXNzdWVkQXQiOiIyMDI1LTA5LTA1VDA1OjM2OjQ0LjAwMFoiLCJ1c2VyQXV0aFRpbWUiOiIyMDI1LTA5LTA1VDA1OjM1OjQ0LjAwMFoiLCJ0b2tlblZlcnNpb24iOiJBenVyZSBBRCBCMkMgdjEiLCJ1c2VyVHlwZSI6IlBPUlRBTCJ9LCJpYXQiOjE3NTcwNTA2MDUsImV4cCI6MTc4ODYwODIwNX0.EuvSN9LkBIehQmS1kygasRnpmY_IvVe2Tdmt_rhXgXo',
+            resource: 'user',
+            action: 'read',
+            context: {
+              userId: '6888b5dc60c798b097e86c91'
+            },
+          }).then(res => {
+            console.log('res=============>', res);
+          }).catch(err => {
+            console.log('err=============>', err);
+          });
+        }
         setIsSignedIn(!!token);
         // setIsSignedIn(true);
       } finally {
@@ -28,7 +43,7 @@ function App() {
     checkAuth();
   }, []);
 
-  const handleLogin = async() => {
+  const handleLogin = async () => {
     // console.log('Hello world');
     await setBearerToken('Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjg4OGI1ZGM2MGM3OThiMDk3ZTg2YzkxIiwidXNlckVtYWlsIjoiZmF6YWxhemltMjM4QGdtYWlsLmNvbSIsInVzZXJGdWxsTmFtZSI6IkZhemFsIEF6aW0iLCJ1c2VyTWljcm9zb2Z0SWQiOiJhOWY3YzkzMS03MTA0LTQyMGItYTdiNS00MjAzMjc4YjliNGMiLCJ1c2VyTWVtYmVyTnVtYmVyIjpudWxsLCJ1c2VyTW9iaWxlUGhvbmUiOm51bGwsInVzZXJQb2xpY3kiOiJCMkNfMV9wcm9qZWN0c2hlbGwiLCJ1c2VySXNzdWVkQXQiOiIyMDI1LTA5LTA1VDA1OjM2OjQ0LjAwMFoiLCJ1c2VyQXV0aFRpbWUiOiIyMDI1LTA5LTA1VDA1OjM1OjQ0LjAwMFoiLCJ0b2tlblZlcnNpb24iOiJBenVyZSBBRCBCMkMgdjEiLCJ1c2VyVHlwZSI6IlBPUlRBTCJ9LCJpYXQiOjE3NTcwNTA2MDUsImV4cCI6MTc4ODYwODIwNX0.EuvSN9LkBIehQmS1kygasRnpmY_IvVe2Tdmt_rhXgXo');
     // signInWithAzureB2C()
